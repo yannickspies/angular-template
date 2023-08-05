d399c9bd7008da54b8a4a434d275edf9345d2973
Yannick <yannickspies91@gmail.com>
2023-08-05 22:33:25 +0200

    generated shared module incl. button

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ButtonComponent {
+
+}
